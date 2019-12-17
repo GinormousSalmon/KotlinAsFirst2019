9bfe950d24408b8106bfeea9e87d524f85bf0ae1
GinormousSalmon <terleczcky.alex@gmail.com>
2019-12-17 22:22:31 +0300

    some solutions in geometry

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -4,10 +4,8 @@ package lesson8.task1
 
 import lesson1.task1.sqr
 import lesson2.task2.pointInsideCircle
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
+import java.lang.IllegalArgumentException
+import kotlin.math.*
 
 /**
  * Точка на плоскости
@@ -120,7 +118,12 @@ fun diameter(vararg points: Point): Segment = TODO()
  * Построить окружность по её диаметру, заданному двумя точками
  * Центр её должен находиться посередине между точками, а радиус составлять половину расстояния между ними
  */
-fun circleByDiameter(diameter: Segment): Circle = TODO()
+fun circleByDiameter(diameter: Segment): Circle {
+    val x = (diameter.begin.x + diameter.end.x) / 2
+    val y = (diameter.begin.y + diameter.end.y) / 2
+    val r = sqrt((diameter.end.x - diameter.begin.x).pow(2) + (diameter.end.y - diameter.begin.y).pow(2)) / 2
+    return Circle(Point(x, y), r)
+}
 
 /**
  * Прямая, заданная точкой point и углом наклона angle (в радианах) по отношению к оси X.
@@ -159,21 +162,33 @@ class Line private constructor(val b: Double, val angle: Double) {
  *
  * Построить прямую по отрезку
  */
-fun lineBySegment(s: Segment): Line = TODO()
+fun lineBySegment(s: Segment): Line {
+    val ang = atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x))
+    return Line(s.begin, ang)
+}
 
 /**
  * Средняя
  *
  * Построить прямую по двум точкам
  */
-fun lineByPoints(a: Point, b: Point): Line = TODO()
+fun lineByPoints(a: Point, b: Point): Line {
+    val ang = atan((b.y - a.y) / (b.x - a.x))
+    return Line(a, ang)
+}
 
 /**
  * Сложная
  *
  * Построить серединный перпендикуляр по отрезку или по двум точкам
  */
-fun bisectorByPoints(a: Point, b: Point): Line = TODO()
+fun bisectorByPoints(a: Point, b: Point): Line {
+    val x = (a.x + b.x) / 2
+    val y = (a.y + b.y) / 2
+    var ang = atan((b.y - a.y) / (b.x - a.x))
+    ang = (ang + PI / 2) % PI
+    return Line(Point(x, y), ang)
+}
 
 /**
  * Средняя
